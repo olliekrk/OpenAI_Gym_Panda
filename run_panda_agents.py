be978e43fccf92ec8829eca3ead61e97a689e733
darkwizz <psyhoge@gmail.com>
2021-06-30 02:34:08 +0200

    add plot for timesteps per episode in run_panda_agents.py, update .gitignore

--- run_panda_agents.py
+++ run_panda_agents.py
@@ -6,8 +6,16 @@ from panda_agents import panda_ppo
 agent, runner = panda_ppo.get_agent_and_runner()
 runner.run(num_episodes=panda_ppo.EPISODES)
 
+plt.figure(0)
 plt.plot(runner.episode_returns)
 plt.title('PPO rewards')
 plt.xlabel('Episodes')
 plt.ylabel('Rewards')
 plt.savefig('ppo-rewards.png', format='png')
+
+plt.figure(1)
+plt.plot(runner.episode_timesteps, 'C1')
+plt.title('PPO timesteps per episode')
+plt.xlabel('Episode')
+plt.ylabel('Timesteps')
+plt.savefig('ppo-timesteps.png', format='png')
