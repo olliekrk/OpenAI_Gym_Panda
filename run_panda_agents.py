9ff3a8f4173066e771eda42e3ffaea32b42821dc
darkwizz <psyhoge@gmail.com>
2021-07-06 00:18:37 +0200

    add episodes number to command line args for run_panda_agents

--- run_panda_agents.py
+++ run_panda_agents.py
@@ -17,7 +17,10 @@ parser = argparse.ArgumentParser()
 parser.add_argument('policy', choices=[
     'ppo', 'ddpg', 'actor'
 ], help=policy_help, metavar='policy')
-parser.add_argument('--max_timesteps', type=int, required=False)
+parser.add_argument('--max_timesteps', type=int, required=False,
+help='maximum number of timesteps per episode', metavar='')
+parser.add_argument('--episodes', type=int, required=False,
+help='number of episodes when learning', metavar='')
 args = parser.parse_args()
 
 POLICY_CHOICES = {
@@ -30,8 +33,10 @@ rs = RandomState(MT19937(SeedSequence(52136)))
 policy = args.policy
 max_episode_timesteps = args.max_timesteps
 chosen_policy = POLICY_CHOICES[policy]
+episodes = args.episodes if args.episodes is not None else chosen_policy.EPISODES
+
 agent, runner = chosen_policy.get_agent_and_runner(max_episode_timesteps)
-runner.run(num_episodes=chosen_policy.EPISODES)
+runner.run(num_episodes=episodes)
 
 plt.figure(0)
 plt.plot(runner.episode_returns)
